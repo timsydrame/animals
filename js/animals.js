97453db72918d9303ce7d8319044c281b42c543a
Colas Rabiller <colas.rabiller@gmail.com>
2023-02-13 19:39:48 +0100

    attemptJS

--- js/animals.js
+++ js/animals.js
@@ -31,7 +31,6 @@ function loadXMLDoc() {
 
     if (this.readyState == 4 && this.status == 200) {
       // console.log(this);
-      // paraf.innerHTML = this.responseText;
       fn_writeXML(this);
     }
   };
@@ -64,30 +63,45 @@ function fn_writeXML(xml) {
     let progressBarSpeed = document.querySelector(".progressBarSpeed");
 
     parentElement.innerHTML += `<div class="border border-light col-12 col-lg-5 d-flex flex-column no-wrap justify-content-center align-items-center m-1 p-2 text-light cardSectionElements">
-                                  <div class="card text-bg-dark overflow-scroll animalCards">
-                                    <img src="${imgElement}" class="card-img" alt="${titreElement}picture">
-                                    <div class="card-img-overlay d-flex flex-column">
-                                        <h5 class="card-title">${titreElement}</h5>
-                                        <div class="d-flex justify-content-between">
-                                          <div>
-                                            <p class="card-text">Weight: <br> ${pWeight} kg
-                                            <br> Size : <br> ${pSize} m</p>
-                                          </div>
-                                          <div>
-                                            <p class="card-text">Speed : <br> ${pSpeed} km/h</p>
-                                          </div>
-                                          <div>
-                                            <p class="card-text">Diet : <br> ${pDiet}
-                                            <br> Location : <br>${pLocation}</p>
-                                          </div>
-                                        </div>
-                                    </div>
-                                      <form action="./php/delete.php" method="post"  class="d-flex justify-content-center align-items-center">
-                                        <input type="hidden" name="cardToDelete" value="${titreElement}"/>
-                                        <button type="submit" class="deleteButton">X</button>
-                                      </form>   
-                                    </div>
-                                </div>`;
+                  <div class="card text-bg-dark overflow-scroll cardsAnimals">
+                    <img src="${imgElement}" class="card-img" alt="${titreElement}picture">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <h5 class="card-title">${titreElement}</h5>
+                        <div class="d-flex justify-content-between">
+                          <div>
+                            <p class="card-text">Weight: <br> ${pWeight} kg
+                            <br> Size : <br> ${pSize} m</p>
+                          </div>
+                          <div>
+                            <p class="card-text">Speed : <br> ${pSpeed} km/h</p>
+                          </div>
+                          <div>
+                            <p class="card-text">Diet : <br> ${pDiet}
+                            <br> Location : <br>${pLocation}</p>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-around">  
+                <button type="submit" class="updateButton">Update</button>
+                <form action="./php/delete.php" method="post"  class="d-flex justify-content-center align-items-center">
+                  <input type="hidden" name="cardToDelete" value="${titreElement}"/>
+                  <button type="submit" class="deleteButton">X</button>
+                </form> 
+              </div>
+                <form action="./php/update.php" method="post" class="updateForm">
+                  <input class="formInput" type="hidden" placeholder="name" name="cardToUpdate"value="${titreElement}"><br>
+                  <input class="formInput" type="text" placeholder="weight" name="weight"><br>
+                  <input class="formInput" type="text" placeholder="size" name="size"><br>
+                  <input class="formInput" type="text" placeholder="speed" name="speed"><br>
+                  <input class="formInput" type="text" placeholder="diet" name="diet"><br>
+                  <input class="formInput" type="text" placeholder="location" name="location"><br>
+                  <input class="formInput" type="text" placeholder="picture url" name="picture"><br>
+                  <div class="d-flex justify-content-center align-items-center p-2">
+                    <button type="submit" class="w-22 update">Update</button>
+                  </div>
+                  </form>
+              </div>
+              </div>`;
 
     console.log(titreElement);
     // Bootstrap progress bar
@@ -100,6 +114,16 @@ function fn_writeXML(xml) {
     labelsData.push(titreElement);
     dataS.push(pWeight);
   }
+
+  let updateForm = document.querySelector(".updateForm");
+  let cardAnimal = document.querySelector(".cardAnimals");
+  let updateBtn = document.querySelector(".updateButton");
+
+  updateBtn.addEventListener("click", () => {
+    console.log("coucou");
+    cardAnimal.style.display = "none !important";
+    updateForm.style.display = "block !important";
+  });
 
   // Customize text color to white and font size
   Chart.defaults.color = "#fff";
